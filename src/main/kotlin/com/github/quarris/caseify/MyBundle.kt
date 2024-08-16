f2114e68440963ae1c0be18c498b1316e3509c11
GitHub Action <action@github.com>
2024-08-16 16:12:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/quarris/caseify/MyBundle.kt
+++ src/main/kotlin/com/github/quarris/caseify/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.quarris.caseify
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/quarris/caseify/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/quarris/caseify/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.quarris.caseify.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
